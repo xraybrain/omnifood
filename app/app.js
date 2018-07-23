f6218f6c514a44b28dc049ff8e688d6701c2794c
xraybrain <xraybrain@gmail.com>
2018-07-23 16:13:29 +0100

    app routers renamed to routes

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ app.set('views', 'app/views');
 // the public dir to be used by all routers
 app.use(express.static('app/public/'));
 // the page to display once the user visits the root dir
-app.use(require('./routers/index.js'));
+app.use(require('./routes/index.js'));
 
 app.listen(app.get('port'), () => {
     console.log('Server listening on port ' + app.get('port'));
